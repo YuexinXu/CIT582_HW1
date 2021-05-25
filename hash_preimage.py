ade5f5f6c14054a611d674f022015a3f08e83b3e
YuexinXu <yuexinxu@seas.upenn.edu>
2021-05-24 20:31:58 -0700

    add homework 2/2

--- hash_preimage.py
+++ hash_preimage.py
@@ -9,23 +9,22 @@ def hash_preimage(target_string):
     	nonce = uuid.uuid4().bytes
     	hash_nonce = hashlib.sha256(nonce).digest()
 
-    	cur = len(target_string) - 1
-    	if cur < 0:
+    	cur = 0
+    	if cur >= len(target_string):
     		return nonce
     	flag = True
-    	for j in range(len(hash_nonce) - 1, -1, -1):
-    		byte = hash_nonce[j]
+    	for byte in hash_nonce:
     		if not flag:
     			break
-    		for i in range(8):
+    		for i in range(7, -1, -1):
     			if (byte & (1 << i)) == 0:
     				cur_bit = '0'
     			else:
     				cur_bit = '1'
     			if cur_bit == target_string[cur]:
-    				cur -= 1
+    				cur += 1
     			else:
     				flag = False
     				break
-    			if cur < 0:
+    			if cur == len(target_string):
     				return (nonce)
