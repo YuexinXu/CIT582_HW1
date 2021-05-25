01a9cbec97516c1bd23552b88b8b57b6af2dda57
YuexinXu <yuexinxu@seas.upenn.edu>
2021-05-24 20:16:11 -0700

    changed trailing bits

--- hash_preimage.py
+++ hash_preimage.py
@@ -9,22 +9,23 @@ def hash_preimage(target_string):
     	nonce = uuid.uuid4().bytes
     	hash_nonce = hashlib.sha256(nonce).digest()
 
-    	cur = 0
-    	if cur >= len(target_string):
+    	cur = len(target_string) - 1
+    	if cur < 0:
     		return nonce
     	flag = True
-    	for byte in hash_nonce:
+    	for j in range(len(hash_nonce) - 1, -1, -1):
+    		byte = hash_nonce[j]
     		if not flag:
     			break
-    		for i in range(7, -1, -1):
+    		for i in range(8):
     			if (byte & (1 << i)) == 0:
     				cur_bit = '0'
     			else:
     				cur_bit = '1'
     			if cur_bit == target_string[cur]:
-    				cur += 1
+    				cur -= 1
     			else:
     				flag = False
     				break
-    			if cur == len(target_string):
-    				return nonce
+    			if cur < 0:
+    				return (nonce)
